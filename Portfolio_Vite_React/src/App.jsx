e2c256632c1465fe4d712428265f868bb9430793
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2023-12-31 21:20:52 -0500

    reorganization folders

--- Portfolio_Vite_React/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Nav from "./Components/Nav";
-const App = () => {
-  return (
-    <div className="bg-slate-900">
-     <h2> Professional Portfolio</h2>
-    </div>
-  );
-}
-export default App
